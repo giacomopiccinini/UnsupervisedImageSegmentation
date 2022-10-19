0c2e7306862674c01ee7c1103b4ed0208afe2bfe
giacomopiccinini <piccinini.giacomo@gmail.com>
2022-10-19 21:08:28 +0200

    [FEAT] Model is saved after training

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 if __name__ == "__main__": 
 
+    import torch
     from Code.Parser.parser import parse
     from Code.Modules.A_train import train
 
+    # Parse arguments
     args = parse()
 
-    train(args)
+    # Train the model
+    model = train(args)
+
+    # Save the model
+    torch.save(model.state_dict(), args.model_path)
