434813fe46a882b99413daa5873f48c3fc4c4e09
giacomopiccinini <piccinini.giacomo@gmail.com>
2022-10-19 21:13:32 +0200

    [FEAT] chronopy support

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 if __name__ == "__main__": 
 
     import torch
+    from chronopy.Clock import Clock
     from Code.Parser.parser import parse
     from Code.Modules.A_train import train
 
+    clock = Clock()
+
     # Parse arguments
+    clock.lap("Parser")
     args = parse()
 
     # Train the model
+    clock.lap("Segmentation")
     model = train(args)
 
     # Save the model
+    clock.lap("Saving")
     torch.save(model.state_dict(), args.model_path)
+
+    clock.lap()
+    clock.summary()
