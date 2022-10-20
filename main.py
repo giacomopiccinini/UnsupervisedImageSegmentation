2102621df2ea1ef016f121b67f5df1cf94732259
giacomopiccinini <piccinini.giacomo@gmail.com>
2022-10-20 10:11:06 +0200

    [ADD] Add plotting of segmented image

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     from chronopy.Clock import Clock
     from Code.Parser.parser import parse
     from Code.Modules.A_train import train
+    from Code.Modules.B_plot import plot
 
     clock = Clock()
 
@@ -18,6 +19,10 @@ if __name__ == "__main__":
     # Save the model
     clock.lap("Saving")
     torch.save(model.state_dict(), args.model_path)
+
+    # Plot the segmentation for each image in the train set
+    clock.lap("Saving segmentation")
+    plot(model, args)    
 
     clock.lap()
     clock.summary()
