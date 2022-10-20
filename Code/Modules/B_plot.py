ca4fd3f3b6539c8dca414743742ad6667b9c79c5
giacomopiccinini <piccinini.giacomo@gmail.com>
2022-10-20 10:10:42 +0200

    [FIX] Fix plotting

--- Code/Modules/B_plot.py
+++ Code/Modules/B_plot.py
@@ -1,30 +1,59 @@
 import torch
-import numpy as np
 import cv2
-from Code.Utilities.loading_utils import load
+import numpy as np
 
+from tqdm import tqdm
+from torch.utils.data import DataLoader
 
-def plot(image, response_map):
+from Code.Classes.Data import Data
 
-    """Plot segmented image as the model is trained"""
+def plot(model, args):
 
-    # Load training parameters
-    architecture_parameters = load("architecture.yaml")
-    globals().update(architecture_parameters)
+    """ Plot final segmented model"""    
 
-    # Apply argmax on every row, keep only index (= class), throw away value
-    _, index = torch.max(response_map, 1)
+    use_cuda = torch.cuda.is_available()
 
-    im_target = index.data.cpu().numpy()
-    labels = len(np.unique(im_target))
-    n_classes = len(np.unique(im_target))
+    # Enable CUDA
+    if use_cuda:
+        model.cuda()
+    model.train()
+        
+    device = torch.device('cuda:0' if use_cuda else 'cpu')
+    model.to(device = device)
 
-    label_colours = np.random.randint(255, size=(n_classes, 3))
+    # Set model to evaluation to make sure layers behave correctly in inference
+    model.eval()
 
-    im_target_rgb = np.array([label_colours[c % n_classes] for c in im_target])
-    im_target_rgb = im_target_rgb.reshape(image.shape).astype(np.uint8)
-    cv2.imwrite("Output/segmentation.png", im_target_rgb)
-    cv2.imshow("output", im_target_rgb)
-    cv2.waitKey(10)
+    # Load dataset and loader for PyTorch
+    dataset = Data(args.path)
+    loader = DataLoader(dataset)
 
-    return labels
+    for batch, names in tqdm(loader):
+        
+        batch = batch.to(device = device)
+
+        # Pass image through SegNet
+        response_map = model(batch)
+
+        # Apply argmax on every row, keep only index (= class), throw away value
+        _, indexed_images = torch.max(response_map, 1)
+
+        # Convert the batch of images into numpy arrays
+        segmented_images = indexed_images.data.cpu().numpy()
+
+        # Ensure data type is correct (they are just masks)
+        segmented_images = segmented_images.astype('uint8')
+
+        for segmented_image, name in zip(segmented_images, names):
+
+            # Find number of distinct classes
+            n_classes = len(np.unique(segmented_image))
+
+            # Assign them a random colour
+            label_colours = np.random.randint(255, size=(n_classes, 3))
+
+            # Colour the image accordingly
+            segmented_image = np.array([label_colours[c % n_classes] for c in segmented_image])
+
+            # Save images
+            cv2.imwrite(f"Output/segmentation.png", segmented_image)
